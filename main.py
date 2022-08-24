d647547f303cba2744e3c5cfe88ed5a9f08d3495
MahirSheikh <72024754+MahirSheikh@users.noreply.github.com>
2022-08-24 10:40:24 +0300

    draw,win,move functions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from turtle import position
+
+
 board = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ',
          6: ' ', 7: ' ', 8: ' ', 9: ' ', 10: ' ',
          11: ' ', 12: ' ', 13: ' ', 14: ' ', 15: ' ',
@@ -24,13 +27,39 @@ def spaceIsFree(board, position):
         return False
 
 def checkWinner(board):
-    pass
+    if board[1] == board[2] == board[3] == board[4] == board[5] == ' ':
+        return True
+    elif board[6] == board[7] == board[8] == board[9] == board[10] == ' ':
+        return True
+    elif board[11] == board[12] == board[13] == board[14] == board[15] == ' ':
+        return True
+    elif board[16] == board[17] == board[18] == board[19] == board[20] == ' ':
+        return True
+    elif board[21] == board[22] == board[23] == board[24] == board[25] == ' ':
+        return True
+    elif board[1] == board[6] == board[11] == board[16] == board[21] == ' ':
+        return True
+    elif board[2] == board[7] == board[12] == board[17] == board[22] == ' ':
+        return True
+    elif board[3] == board[8] == board[13] == board[18] == board[23] == ' ':
+        return True
+    elif board[4] == board[9] == board[14] == board[19] == board[24] == ' ':
+        return True
+    elif board[5] == board[10] == board[15] == board[20] == board[25] == ' ':
+        return True
+    elif board[1] == board[7] == board[13] == board[19] == board[25] == ' ':
+        return True
+    elif board[5] == board[9] == board[13] == board[17] == board[21] == ' ':
+        return True
+    else:
+        return False
+
 def checkDraw(board):
-    pass
-    # if ' ' in board.values():
-    #     return False
-    # else:
-    #     return True
+    for k in board.keys():
+        if board[k] == ' ':
+            return False
+    return True
+
 
 def insertLetter(letter, board, position):
     if spaceIsFree(board, position):
@@ -53,5 +82,18 @@ def insertLetter(letter, board, position):
         position=int(input('Choose a different position:\t'))
         insertLetter(letter, board, position)
 
-insertLetter('o', board, 1)
-insertLetter('o', board, 1)
+def playerMove():
+    position = int(input('Choose a position for O:\t'))
+    insertLetter('o', board, position)
+
+def botMove():
+    position = int(input('Choose a position for X:\t'))
+    insertLetter('x', board, position)
+
+while not checkWinner():
+    botMove()
+    playerMove()
+
+
+
+
